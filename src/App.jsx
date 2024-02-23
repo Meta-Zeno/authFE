7001316c544e99dcb02f741fb5712cead85f6be5
Mike <147070237+Meta-Zeno@users.noreply.github.com>
2024-02-23 12:11:34 +0000

    Revert "Enhancemenmts: LogOrSign, GetAllUsers completed to  basic requirements"

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import LogOrSign from "./components/logOrSign/LogOrSign";
 
 import "./App.css";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    console.log("useeffect hit");
-    async function fetchusers() {
-      console.log("fetchusers");
-      const response = await fetch("http://localhost:5001/users/getAllUsers", {
-        method: "GET",
-        mode: "cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await response.json();
-      await setUsers(data.users);
-    }
-    fetchusers();
-  }, []);
-
   return (
     <>
       <div className="wrapper">
-        <LogOrSign setLoggedIn={setLoggedIn} />
-
-        {!loggedIn ? <p>Please log in</p> : <p>hello {loggedIn.username}</p>}
-        {users.length > 0 ? users.map((user) => <p>{user.username}</p>) : null}
+        <LogOrSign />
       </div>
     </>
   );
