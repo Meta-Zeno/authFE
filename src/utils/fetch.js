d050644a7bc561a7fa468f22af56ce91cb0638fd
Michael Gregory <mr.m.gregory@outlook.com>
2024-02-23 12:08:01 +0000

    Enhancemenmts: LogOrSign, GetAllUsers completed to  basic requirements

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -15,3 +15,22 @@ export const signupUser = async (username, email, password) => {
   const data = await response.json();
   console.log("data in fetch sign up", data);
 };
+
+export const loginUser = async (username, password) => {
+  console.log("Login user", username, password);
+  const response = await fetch("http://localhost:5001/users/login", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+  });
+  console.log("responce", response);
+  const data = await response.json();
+  console.log("stringdata", data);
+  return data;
+};
